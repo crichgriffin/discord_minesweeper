8d9a38dfecd4c7a88de8dcb6ad241f4b2e482753
charlotterich <25774602+charlotterich@users.noreply.github.com>
2020-07-03 18:20:02 +0100

    got rid of stupid old version

--- minesweeper.py
+++ minesweeper.py
@@ -6,40 +6,6 @@ num2words = {0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', \
             15: 'fifteen', 16: 'sixteen', 17: 'seventeen', 18: 'eighteen', 19: 'nineteen'}
 
 
-def minesweeper(n, k):
-    arr = [[0 for row in range(n)] for column in range(n)]
-    for num in range(k):
-        x = random.randint(0, n - 1)
-        y = random.randint(0, n - 1)
-        arr[y][x] = 'X'
-        if (x >= 0 and x <= 3) and (y >= 0 and y <= 4):
-            if arr[y][x + 1] != 'X':
-                arr[y][x + 1] += 1  # center right
-        if (x >= 1 and x <= 4) and (y >= 0 and y <= 4):
-            if arr[y][x - 1] != 'X':
-                arr[y][x - 1] += 1  # center left
-        if (x >= 1 and x <= n - 1) and (y >= 1 and y <= n - 1):
-            if arr[y - 1][x - 1] != 'X':
-                arr[y - 1][x - 1] += 1  # top left
-
-        if (x >= 0 and x <= n - 2) and (y >= 1 and y <= n - 1):
-            if arr[y - 1][x + 1] != 'X':
-                arr[y - 1][x + 1] += 1  # top right
-        if (x >= 0 and x <= n - 1) and (y >= 1 and y <= n - 1):
-            if arr[y - 1][x] != 'X':
-                arr[y - 1][x] += 1  # top center
-
-        if (x >= 0 and x <= n - 2) and (y >= 0 and y <= n - 2):
-            if arr[y + 1][x + 1] != 'X':
-                arr[y + 1][x + 1] += 1  # bottom right
-        if (x >= 1 and x <= n - 1) and (y >= 0 and y <= n - 2):
-            if arr[y + 1][x - 1] != 'X':
-                arr[y + 1][x - 1] += 1  # bottom left
-        if (x >= 0 and x <= n - 1) and (y >= 0 and y <= n - 2):
-            if arr[y + 1][x] != 'X':
-                arr[y + 1][x] += 1  # bottom center
-     return(arr)
-
 
 def place_mines(n,k):
     arr = [[0 for row in range(n)] for column in range(n)]
